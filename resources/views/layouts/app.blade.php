eb6e1623755ec656716deeb53733a961554ba668
Samuel Guerrero <samuelg510@gmail.com>
2023-03-18 22:48:18 -0500

    Creando un Layout Principal y utilizandolo

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>DevStagram - @yield('titulo')</title>
+    </head>
+    <body>
+        <h1>@yield('titulo')</h1>        
+        @yield('contenido')
+        <hr>
+        <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
+            Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+        </div>
+    </body>
+</html>
---
+++ resources/views/principal.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Página principal
+@endsection
+@section('contenido')
+    Página contenido
+@endsection
