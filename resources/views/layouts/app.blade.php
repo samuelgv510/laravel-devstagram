53f684225046d9762dec27a4bfd1a7255f4777fa
Samuel Guerrero <samuelg510@gmail.com>
2023-03-19 20:40:53 -0500

    add RegisterController

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
             <h2 class="font-black text-center text-3xl mb-10">@yield('titulo')</h2>
             @yield('contenido')
         </main> 
-        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+        <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
             DevStagram - Todos los derechos reservados {{now()->year}}
         </footer>
         <hr>
